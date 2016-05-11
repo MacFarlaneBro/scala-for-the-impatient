3e675c284a33d43ccfb2bde6da82d3ee828fb1ff
Charlie Macfarlane Brodie <charliembrodie@gmail.com>
2016-05-11 19:28:10 +0100

    Completed Exercise 9.

--- chapter4/exercise9.scala
+++ chapter4/exercise9.scala
@@ -2,8 +2,12 @@ object exercise9 {
 
   val intArray = Array(1, 2, 4, 6, 12, 64, 12, 6, 1, 0, -1, 32, -89)
 
-  def lteqgt(values: Array[Int], v: Int): Tuple2[Int, Int, Int] = {
-
+  def lteqgt(values: Array[Int], v: Int): Tuple3[Int, Int, Int] = {
+    (intArray.count(_ > v),
+    intArray.count(_ == v),
+    intArray.count(_ < v))
   }
+
+  println(lteqgt(intArray, 12))
 }
 
