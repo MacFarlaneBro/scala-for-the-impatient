81af17f151ea06eb9aed8802881fff5b677988ca
Charlie Macfarlane Brodie <charliembrodie@gmail.com>
2016-05-11 13:20:45 +0100

    Fixed Ex8 and started Ex9.

--- chapter4/exercise8.scala
+++ chapter4/exercise8.scala
@@ -3,7 +3,7 @@ object exercise8 {
   val intArray = Array(1, 2, 4, 6, 12, 64, 12, 6, 1, 0, -1, 32, -89)
 
   def minMax(array: Array[Int]): Tuple2[Int, Int] = {
-    return (array.min, array.max)
+    (array.min, array.max)
   }
 
   println(minMax(intArray))
---
+++ chapter4/exercise9.scala
@@ -0,0 +1,9 @@
+object exercise9 {
+
+  val intArray = Array(1, 2, 4, 6, 12, 64, 12, 6, 1, 0, -1, 32, -89)
+
+  def lteqgt(values: Array[Int], v: Int): Tuple2[Int, Int, Int] = {
+
+  }
+}
+
