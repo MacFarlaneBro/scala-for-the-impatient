64246b43cd468d29f6e166e4618b77a8b8cb1e07
charlie <charliembrodie@gmail.com>
2016-05-14 20:33:48 +0100

    Finished exercise7.

---
+++ chapter5/exercise7.scala
@@ -0,0 +1,11 @@
+class Person(name: String) {
+
+  val firstName = name.split(" ")(0)
+  val secondName = name.split(" ")(1)
+
+}
+
+/*
+ The parameter should be a plain parameter as it is only needed in the initialisation of the object, this way it is discarded
+ and cannot be accessed at a later time.
+ */
