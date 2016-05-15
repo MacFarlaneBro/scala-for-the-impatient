c82b5288c2de9ba5fe37a1a3f21c110f54ee8bd0
charlie <charliembrodie@gmail.com>
2016-05-15 18:26:37 +0100

    Completed exercise 1.

---
+++ chapter6/exercise1.scala
@@ -0,0 +1,14 @@
+object Conversions {
+
+  def inchesToCentimeters(inches: Double):Double = {
+    inches * 2.54
+  }
+
+  def gallonsToLiters(gallons: Double):Double = {
+    gallons * 4.54
+  }
+
+  def milesToKilometers(miles: Double):Double = {
+    miles * 1.6
+  }
+}
