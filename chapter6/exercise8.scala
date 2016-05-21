67c36c0ac9a30017b3dacde3e1aa03f0b7968e72
Charlie Macfarlane Brodie <charliembrodie@gmail.com>
2016-05-21 14:09:57 +0100

    Completed exercise 8.
    Didn't know what a colour cube was. It really does seem like he's
    reaching quite a bit to try and fill question space on some of these chapters.

---
+++ chapter6/exercise8.scala
@@ -0,0 +1,11 @@
+//No idea what a colour cube is, had to google this one, seems kind of dumb to ask about something which quite clearly depended on some kind of outside knowledge.
+object ColourCube extends Enumeration {
+    val black = Value(0x000000)
+    val red = Value(0xff0000)
+    val green = Value(0x00ff00)
+    val blue = Value(0x0000ff)
+    val yellow = Value(0xffff00)
+    val magenta = Value(0xff00ff)
+    val cyan = Value(0x00ffff)
+    val white = Value(0xffffff)
+}
